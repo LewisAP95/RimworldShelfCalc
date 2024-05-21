91200c4a8e487d341907e26f4b5e9d881408fe0d
Lewis Perry <LewisA.Perry@hotmail.co.uk>
2024-05-21 17:33:08 +0100

    Basic layout starting setup.

--- ShelfCalc.py
+++ ShelfCalc.py
@@ -1,5 +1,33 @@
+import tkinter as tk
+
 class MainWindow:
-    pass
+
+    def __init__(self):
+        self.app = tk.Tk()
+        self.app.title("RimWorld Shelf Layout Calculator")
+
+        self.left_frame = tk.Frame(self.app, relief="groove", borderwidth=3)
+        self.left_frame.grid(row=0, column=0)
+
+        self.right_frame = tk.Frame(self.app, relief="groove", borderwidth=3)
+        self.right_frame.grid(row=0, column=1)
+
+        self.left_frame_top = tk.Frame(self.left_frame)
+        self.left_frame_top.grid(row=0, column=0)
+        self.left_frame_bottom = tk.Frame(self.left_frame, relief="ridge", borderwidth=2)
+        self.left_frame_bottom.grid(row=1, column=0)
+
+        self.testlabel1 = tk.Label(self.left_frame_top, text="main left inner upper")
+        self.testlabel2 = tk.Label(self.left_frame_bottom, text="main left inner lower")
+        self.testlabel1.pack()
+        self.testlabel2.pack()
+
+        self.testlabel3 = tk.Label(self.right_frame, text="main right")
+        self.testlabel3.pack()
+
+    def begin(self):
+        self.app.mainloop()
 
 if __name__=="__main__":
     w = MainWindow()
+    w.begin()
